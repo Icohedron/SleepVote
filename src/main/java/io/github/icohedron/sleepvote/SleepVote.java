c9646e526eb4497321bde60e2777a3a52c7f47d0
Icohedron <icohedron@gmail.com>
2017-04-05 22:34:33 -0600

    Refactor, Fix bugs, Add a few features
    Getting ready for pre-release v0.4.0

--- src/main/java/io/github/icohedron/sleepvote/SleepVote.java
+++ src/main/java/io/github/icohedron/sleepvote/SleepVote.java
@@ -2,157 +2,213 @@ package io.github.icohedron.sleepvote;
 
 import com.google.inject.Inject;
 import ninja.leaping.configurate.ConfigurationNode;
-import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
-import org.spongepowered.api.Game;
-import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.action.SleepingEvent;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.world.World;
 import org.spongepowered.api.world.storage.WorldProperties;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
-@Plugin(id = "sleepvote", name = "Sleep Vote")
+@Plugin(id = "sleepvote", name = "SleepVote")
 public class SleepVote {
 
-    private static final float  DEFAULT_PERCENT = 0.5f;
-    private static final String DEFAULT_MESSAGE = "Wakey wakey, rise and shine!";
+    private static final String PLUGIN_ID = "sleepvote";
 
     @Inject
-    @DefaultConfig(sharedRoot = true)
-    private Path defaultConfigPath;
-
-    private ConfigurationLoader<CommentedConfigurationNode> configLoader;
-    private ConfigurationNode rootNode;
+    @ConfigDir(sharedRoot = true)
+    private Path configurationDirectory;
 
     @Inject
     private Logger logger;
 
-    @Inject
-    private Game game;
-
     private Map<World, Set<Player>> sleeping;
-    private float percent;
-    private String successMessage;
+    private Map<Player, Task> playerSleepRequests;
+
+    private boolean enablePrefix;
+    private boolean messageLogging;
+
+    private float requiredPercentSleeping;
+    private String wakeupMessage;
+    private String enterBedMessage;
+    private String exitBedMessage;
 
     @Listener
-    public void onInitiation(GameInitializationEvent event) {
+    public void onInitializationEvent(GameInitializationEvent event) {
+        configurationDirectory = configurationDirectory.resolve(PLUGIN_ID);
         sleeping = new HashMap<>();
-        configLoader = HoconConfigurationLoader.builder().setPath(defaultConfigPath).build();
-        try {
-            rootNode = configLoader.load();
-        } catch(IOException e) {
-            logger.error("Failed to read configuration file (improper syntax?)! Falling back to default configuration...");
-            percent = DEFAULT_PERCENT;
-            successMessage = DEFAULT_MESSAGE;
-            return;
-        }
-
-        if (rootNode.getChildrenMap().isEmpty()) {
-            logger.info("No configuration file detected. Creating default configuration...");
-            createDefaultConfig();
-        } else {
-            percent = rootNode.getNode("percent").getFloat();
-            successMessage = rootNode.getNode("success_message").getString();
-
-            logger.info("Got percent value of " + percent + " and success_message value of \"" + successMessage + "\"");
-        }
+        playerSleepRequests = new HashMap<>();
+        loadConfiguration();
+        // TODO: Commands (e.g. '/sleepvote reload', '/sleepvote ignore <player>', '/sleepvote unignore <player>')
     }
 
-    @Listener
-    public void onPlayerSleepPre(SleepingEvent.Pre event) {
-        Optional<Player> player = event.getCause().first(Player.class);
-        if (!player.isPresent()) {
-            return;
-        }
+    private void loadConfiguration() {
+        Path settingsPath = configurationDirectory.resolve(PLUGIN_ID + ".conf");
 
-        World world = player.get().getWorld();
-        if (!sleeping.containsKey(world)) {
-            sleeping.put(world, new HashSet<>());
-        }
+        ConfigurationLoader defaultConfigurationLoader = HoconConfigurationLoader.builder().setURL(Sponge.getAssetManager().getAsset(this, "default_" + PLUGIN_ID + ".conf").get().getUrl()).build();
+        Optional<ConfigurationNode> defaultRootNode = getRootNode(defaultConfigurationLoader);
 
-        long localWorldTime = world.getProperties().getWorldTime() % 24000;
-        if (!(localWorldTime >= 12541 && localWorldTime <= 23458)) {
-            return;
-        }
+        ConfigurationLoader configurationLoader = HoconConfigurationLoader.builder().setPath(settingsPath).build();
+        Optional<ConfigurationNode> rootNode = getRootNode(configurationLoader);
 
-        if (sleeping.get(world).add(player.get())) {
-            String message = player.get().getName() + " wants to sleep! " + sleeping.get(world).size() + "/" + requiredPlayerCount(world);
-            worldMessage(world, message);
-        }
-    }
+        if (!rootNode.filter(node -> !node.isVirtual()).isPresent()) {
 
-    @Listener
-    public void onPlayerSleepPost(SleepingEvent.Post event) {
-        Optional<Player> player = event.getCause().first(Player.class);
-        if (!player.isPresent()) {
-            return;
-        }
-
-        World world = player.get().getWorld();
-        if (sleeping.get(world).remove(player.get())) {
-            String message = player.get().getName() + " left their bed! " + sleeping.get(world).size() + "/" + requiredPlayerCount(world);
-            worldMessage(world, message);
-        }
-    }
-
-    @Listener
-    public void onPlayerSleepTick(SleepingEvent.Tick event) {
-        for (World world : sleeping.keySet()) {
-            if (sleeping.get(world).isEmpty()) {
-                continue;
+            if (Files.notExists(settingsPath)) { // Probably the case if the root node is virtual.
+                logger.info("No configuration file detected. Creating configuration file...");
+                try {
+                    Files.createDirectories(configurationDirectory);
+                    Sponge.getAssetManager().getAsset(this, "default_" + PLUGIN_ID + ".conf").get().copyToFile(settingsPath);
+                    logger.info("Finished! Configuration file saved to \"" + settingsPath.toString() + "\"");
+                } catch (IOException e) {
+                    logger.error("Failed to create configuration file! Aborting operation and falling back to default configuration.");
+                    e.printStackTrace();
+                }
+            } else { // Otherwise, there was likely a parsing error.
+                logger.error("Failed to read configuration file! (Improper syntax?) Falling back to default configuration.");
             }
-            if (sleeping.get(world).size() >= requiredPlayerCount(world)) {
-                setTicksToMorningRelativeToDaysAlive(world.getProperties());
-                worldMessage(world, successMessage);
-                sleeping.get(world).removeAll(sleeping.get(world));
-            }
+
+            // In both cases, just load the default configuration.
+            rootNode = defaultRootNode;
+        }
+
+        ConfigurationNode root = rootNode.get();
+        enablePrefix = root.getNode("sleepvote_prefix").getBoolean();
+        messageLogging = root.getNode("enable_logging").getBoolean();
+        requiredPercentSleeping = root.getNode("required_percent_sleeping").getFloat();
+        wakeupMessage = root.getNode("messages", "wakeup").getString();
+        enterBedMessage = root.getNode("messages", "enter_bed").getString();
+        exitBedMessage = root.getNode("messages", "exit_bed").getString();
+
+        if (requiredPercentSleeping <= 0.0f || requiredPercentSleeping > 1.0f) {
+            logger.info("Invalid or missing required_percent_sleeping value. Using default of 0.5");
+            requiredPercentSleeping = 0.5f;
+        }
+
+        if (wakeupMessage == null) {
+            logger.info("Missing messages.wakeup string. Using default of \"Wakey wakey, rise and shine!\"");
+            wakeupMessage = "Wakey wakey, rise and shine!";
+        }
+
+        if (enterBedMessage == null) {
+            logger.info("Missing messages.enter_bed string. Using default of \"<player> wants to sleep! <sleeping>/<active> (<percent>%)\"");
+            enterBedMessage = "<player> wants to sleep! <sleeping>/<active> (<percent>%)";
+        }
+
+        if (exitBedMessage == null) {
+            logger.info("Missing messages.exit_bed string. Using default of \"<player> has left their bed. <sleeping>/<active> (<percent>%)\"");
+            exitBedMessage = "<player> has left their bed. <sleeping>/<active> (<percent>%)";
         }
     }
 
-    private void worldMessage(World world, String message) {
-        for (Player p : world.getPlayers()) {
-            p.sendMessage(Text.builder(message).color(TextColors.YELLOW).build());
-        }
-    }
-
-    private void createDefaultConfig() {
-        percent = DEFAULT_PERCENT;
-        successMessage = DEFAULT_MESSAGE;
-
-        rootNode.getNode("percent").setValue(percent);
-        rootNode.getNode("success_message").setValue(successMessage);
-
+    private Optional<ConfigurationNode> getRootNode(ConfigurationLoader configurationLoader) {
         try {
-            configLoader.save(rootNode);
+            return Optional.ofNullable(configurationLoader.load());
         } catch (IOException e) {
-            logger.error("Failed to create default configuration file! Aborting...");
+            logger.error("An error occurred while attempting to read the configuration file!");
+            e.printStackTrace();
+            return Optional.empty();
         }
     }
 
-    private int requiredPlayerCount(World world) {
-        return (int) Math.ceil(world.getPlayers().size() * percent);
+    //// Sleeping Mechanics ////
+
+    @Listener
+    public void onPreSleepingEvent(SleepingEvent.Pre event, @First Player player) {
+        if (playerSleepRequests.containsKey(player)) {
+            playerSleepRequests.get(player).cancel();
+        }
+
+        playerSleepRequests.put(player, Task.builder().execute(() -> {
+            if (isInBed(player)) {
+                World world = player.getWorld();
+                sleeping.computeIfAbsent(world, w -> new HashSet<>());
+                sleeping.get(world).add(player);
+                sendWorldMessage(world, parseMessage(enterBedMessage, world, Optional.of(player)));
+            }
+        }).async().delay(4, TimeUnit.SECONDS).submit(this));
     }
 
-    // Method by FerusGrim -- https://forums.spongepowered.org/t/set-time-to-start-of-day-sleepingevent/9605/2?u=icohedron
-    public void setTicksToMorningRelativeToDaysAlive(WorldProperties properties) {
-        long alive = properties.getWorldTime();
-        double days = alive / 24000;
-        long setTo = ((int) Math.ceil(days)) * 24000;
-        properties.setWorldTime(setTo);
+    @Listener
+    public void onSleepTickEvent(SleepingEvent.Tick event) {
+        for (World world : sleeping.keySet()) {
+            Set<Player> sleepingPlayers = sleeping.get(world);
+            if (!sleepingPlayers.isEmpty()) {
+                int num_sleeping = sleepingPlayers.size();
+                int active = getActivePlayerCount(world);
+                WorldProperties worldProperties = world.getProperties();
+
+                if (num_sleeping >= active) {
+                    worldProperties.setWorldTime(((int) Math.ceil(worldProperties.getWorldTime() / 24000.0f)) * 24000); // Set time to the next multiple 24000 ticks (equivalent to '/time set 0')
+                    sendWorldMessage(world, parseMessage(wakeupMessage, world, Optional.empty()));
+                    sleepingPlayers.clear();
+                }
+            }
+        }
+    }
+
+    @Listener
+    public void onPostSleepingEvent(SleepingEvent.Post event, @First Player player) {
+        World world = player.getWorld();
+        if (sleeping.get(world).remove(player)) {
+            sendWorldMessage(world, parseMessage(exitBedMessage, world, Optional.of(player)));
+        }
+    }
+
+    private Text parseMessage(String message, World world, Optional<Player> player) {
+        String playerName = player.isPresent() ? player.get().getName() : "Undefined";
+        int sleepingPlayers = sleeping.get(world).size();
+        int active = getActivePlayerCount(world);
+        int percent = (int) (sleepingPlayers * 100.0f / active);
+
+        Text prefix = Text.of(TextColors.GREEN, "[", TextColors.RED, "SleepVote", TextColors.GREEN, "] ");
+        Text text = Text.of(TextColors.YELLOW, message.replace("<player>", playerName)
+                               .replace("<sleeping>", Integer.toString(sleepingPlayers))
+                               .replace("<active>", Integer.toString(active))
+                               .replace("<percent>", Integer.toString(percent)));
+
+        Text result = enablePrefix ? Text.of(prefix, text) : text;
+        if (messageLogging) {
+            logger.info("[" + world.getName() + "] " + result.toPlain());
+        }
+        return result;
+    }
+
+    private boolean isInBed(Player player) {
+        // Doesn't work due to bug: https://forums.spongepowered.org/t/warnings-on-startup-skipping-keys/18338
+//        return player.get(Keys.IS_SLEEPING).filter(k -> k.booleanValue()).isPresent();
+
+        // Workaround: takes advantage of the fact that the player's hitbox shrinks to a (almost) perfect 0.2*0.2*0.2 cube while in a bed.
+        // But we only need to check the floored y-values! The player's hitbox is normally greater than 1, so if the floored y-value is not 0, then the player must be in a bed!
+        return player.getBoundingBox().filter(b -> b.getSize().getFloorY() == 0).isPresent();
+    }
+
+    private void sendWorldMessage(World world, Text message) {
+        for (Player p : world.getPlayers()) {
+            p.sendMessage(message);
+        }
+    }
+
+    private int getActivePlayerCount(World world) {
+        // TODO: Add exclusions for specific players (such as vanished, afk, etc.)
+        return (int) Math.ceil(world.getPlayers().size() * requiredPercentSleeping);
     }
 }
